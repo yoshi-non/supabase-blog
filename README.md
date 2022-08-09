5729803fdd3736aadcda4ed062119b843d30fcdf
yoshi-non <himekawa.yoshino44@gmail.com>
2022-08-09 14:05:36 +0900

    finish

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Supabaseチュートリアル
+# Supabaseチュートリアル 簡易ブログ
+
+![supabase-blog](https://user-images.githubusercontent.com/83369665/183567899-05d547f9-208c-4312-8c54-67969c16593b.png)
+
+## Supabaseのサイト
+
+URL: [https://supabase.com/](https://supabase.com/)
 
 ## Supabaseのインストール
 yarn add @supabase/supabase-js
@@ -7,9 +13,14 @@ yarn add @supabase/supabase-js
 yarn dev
 
 ## Supabaseのデータカラム
+
 ![supabase](https://user-images.githubusercontent.com/83369665/183470547-126aca6e-3f93-4a95-bcfb-219b952941d2.png)
 
 作成したプロジェクトの設定からProject URLのURLとProject API keysのKEYを
-utilというフォルダのclient.tsというファイルのSUPABASE_URLとSUPABASE_KEYにそれぞれ割り当てる。
+utilというフォルダのclient.tsというファイルのNEXT_PUBLIC_SUPABASE_URLとNEXT_PUBLIC_SUPABASE_KEYにそれぞれ割り当てる。
 
-中身は.envに記載しgitignoreする。
+中身は.env.localに記載しgitignoreする。
+
+## データベースのテーブル確認
+
+![supabase-table](https://user-images.githubusercontent.com/83369665/183568422-8d4cb8b4-06cc-4e45-a98c-2c690cbb036d.png)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,57 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { supabase } from '../util/client'
 
 const Home: NextPage = () => {
+  const [posts, setPosts] = useState<any[]>([])
+  const [post, setPost] = useState({title: "", content: ""})
+  const {title, content} = post
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  async function fetchPosts() {
+    const {data}: any = await supabase
+      .from("posts")
+      .select()
+    setPosts(data)
+    console.log("data:", data)
+  }
+  async function createPost() {
+    await supabase
+      .from("posts")
+      .insert([
+        {title, content}
+      ])
+      .single()
+    setPost({title: "", content: ""})
+    fetchPosts()
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      Supabase-tutorial
+    <div className="flex min-h-screen flex-col items-center justify-center gap-5">
+      <div className='flex gap-4'>
+        <input
+          className='border border-black rounded'
+          placeholder='Title'
+          value={title}
+          onChange={e => setPost({ ...post, title: e.target.value })}
+        />
+        <input
+          className='border border-black rounded'
+          placeholder='Content'
+          value={content}
+          onChange={e => setPost({ ...post, content: e.target.value })}
+        />
+      </div>
+      <button className='bg-gray-400 p-2 rounded-full' onClick={createPost}>Create Post</button>
+      {
+        posts.map(post => (
+          <div key={post.id} className="text-center text-xl">
+            <h3 className='font-bold text-2xl'>{post.title}</h3>
+            <p>{post.content}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
